eb2193aac818acf268580cd8530947cebe2dd591
Tyler Kluszczynski <tylerkluszczynski@gmail.com>
2019-02-01 15:42:41 -0800

    Getting ready for tutorial.
    Upaded /src/engine.py and /src/moveable.py with some changes to remove 
    errors on Windows machines. 
    
    Updated engine.py to have character as a class parameter when creating 
    the object, instead of passing it as a variable later.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python Roguelike Tutorial 2019
+A tutorial for Python 3 + libtcodpy inspired by the tutorial @TStand90 made in 2017.
+
+### Improvements / Changes
+This tutorial will be similar to the 2017 version with some noteworthy changes.
+
+* More focus on using Object Oriented Programming practices.
+* Special focus to topics such a encapsulation and code reusability (modularization).
+* Every piece of code will be commented in-line.
+* More explanation as to what each line of code does and why we are using it.
+* Tutorial for how to host your project on GitHub (basic Git tutorial).
+
+### Requirements (Covered in Part 0)
+* The latest version of Python 3 - [available here.](https://www.python.org/downloads/)
+* Libtcodpy -  [available here.](https://github.com/libtcod/python-tcod)
+* Libtcodpy documentation can be found [here.](https://python-tcod.readthedocs.io/en/latest/libtcodpy.html)
+* A text editor or IDE (Covered in part 0).
+* Basic programming knowledge - methods, variables, loops, if statements, etc.
+
+### The Tutorial
+* ##### [Part 0 - Environment.](/docs/part-0.md)
+* ##### [Part 1 - It's Alive!](/docs/part-1.md)
--- src/engine.py
+++ src/engine.py
@@ -1,5 +1,5 @@
 # Importing libtcod and our other python files.
-import libtcodpy as libtcod
+import tcod as libtcod
 import inputHandler
 import moveable
 
@@ -12,11 +12,14 @@ WINDOW_TITLE = "Libtcod RL"
 # Main function that runs first.
 def main():
 
-    # Actually creating the screen. We use the global variables we set up earlier.
-    default_console = createScreen(FONT, WINDOW_TITLE)
+    # Creating 2 consoles, the root console then the default console.
+    consoles = consoleSetup()
+
+    root_console = consoles[0]
+    default_console = consoles[1]
 
     # Running function that sets up the player's starting position and '@' symbol.
-    player = setupPlayer(default_console)
+    player = createPlayer(default_console)
 
     # Setting up keyboard and mouse inputs.
     key = libtcod.Key()
@@ -28,42 +31,60 @@ def main():
         # Gets the user input.
         action = inputHandler.handleInput(key,mouse)
 
-        # Handles console updates during runtime.
-        consoleRuntime(default_console)
+        # Performs action based on user input.
+        doPlayerActions(action, player)
 
-        # Handles the action from user input.
-        handleActions(action, player)
+        # Performs console runtime functions (blitting, flushing etc)
+        consoleRuntime(default_console, root_console)
 
 
-# Console functions done during the game loop.
-def consoleRuntime(console):
+
+# Setting up the @ Player.
+def createPlayer(default_console):
+
+    # Getting default starting position based on window size.
+    player_x = int(SCREEN_WIDTH/2)
+    player_y = int(SCREEN_HEIGHT/2)
+
+    # Creating player based on moveable object.
+    # Object has x-position, y-position, console and representitve character.
+    player = moveable.Moveable(player_x, player_y, default_console, '@')
+
+    # Placing the player on the screen on the coordinates specified earlier.
+    player.placeEntity()
+
+    # Returns the player object after it has been created
+    return player
+
+# Function that creates the console_set_default_foreground window for the game.
+def consoleSetup():
+
+    # Setting our font, setting to greyscale, also setting the layout to TCOD
+    libtcod.console_set_custom_font(FONT, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    # Creating the root console.
+    root_console = libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE, False)
+
+    # Creating a main console that gets overlayed on top of the root console.
+    default_console = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     # Setting default console foreground color to white.
-    libtcod.console_set_default_foreground(console, libtcod.white)
+    libtcod.console_set_default_foreground(default_console, libtcod.white)
+
+
+    return [root_console, default_console]
+
+# Function that modifies console during runtime.
+def consoleRuntime(default_console, root_console):
 
     # Blitting the console
-    libtcod.console_blit(console, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    libtcod.console_blit(default_console, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, root_console, 0, 0)
 
     # Redraw console
     libtcod.console_flush()
 
-
-# Function that creates the console window for the game.
-def createScreen(font, title):
-
-    # Setting our font, setting to greyscale, also setting the layout to TCOD
-    libtcod.console_set_custom_font(font, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
-    # Creating the console window.
-    libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, False)
-
-    # Returns the console.
-    console = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-    return console
-
-# Handles user Actions
-def handleActions(action, moveable_object):
+# Gets the user's action
+def doPlayerActions(action, moveable_object):
 
     move = action.get('move')
 
@@ -73,24 +94,9 @@ def handleActions(action, moveable_object):
         coordinates[0] += dx
         coordinates[1] += dy
 
-        moveable_object.updatePosition(coordinates, '@')
+        moveable_object.updatePosition(coordinates)
 
 
-# Setting up the @ Player.
-def setupPlayer(default_console):
-
-    # Getting default starting position based on window size.
-    player_x = int(SCREEN_WIDTH/2)
-    player_y = int(SCREEN_HEIGHT/2)
-
-    # Creating player object
-    player = moveable.Moveable(player_x, player_y, default_console)
-
-    # Placing the player on the screen on the coordinates specified earlier.
-    player.placeEntity('@')
-
-    # Returns the player object after it has been created
-    return player
 
 # Main function only gets ran if this file is called.
 if __name__ == '__main__':
